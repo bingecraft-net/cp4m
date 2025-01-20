52fe0656a95f92956dba27779c8ff8fc3230eceb
tyler <tyler@cruftbusters.com>
2025-01-20 22:17:50 +0000

    parse 'long' (just treat it as an i64)

--- src/snbt.rs
+++ src/snbt.rs
@@ -22,19 +22,18 @@ pub fn parse_snbt(input: String) -> Result<HashMap<String, Value>, Error> {
             index += 1;
         } else {
             match (&state, chars[index]) {
-                (State::Start, '{') => {
-                    state = State::Object;
-                }
-                (State::Object, '}') => {
-                    state = State::End;
-                }
+                (State::Start, '{') => state = State::Object,
+                (State::Object, '}') => state = State::End,
                 (_, '}') => return Err(Error::UnmatchedRightBrace),
                 (State::Object, _) => {
                     let key = read_until(&chars, &mut index, ':');
-                    index += 1;
-                    index += 1;
-                    let value = read_until(&chars, &mut index, '\n');
-                    result.insert(key, Value::Number(value.parse().unwrap()));
+                    index += 2;
+                    let mut value = read_until(&chars, &mut index, '\n');
+                    if value[value.len() - 1] == 'L' {
+                        value.pop();
+                    }
+                    let number = value.iter().collect::<String>().parse().unwrap();
+                    result.insert(key.iter().collect(), Value::Number(number));
                 }
                 _ => todo!("got {:?}", chars[index]),
             }
@@ -55,12 +54,12 @@ enum State {
     End,
 }
 
-fn read_until(chars: &Vec<char>, index: &mut usize, until: char) -> String {
+fn read_until(chars: &Vec<char>, index: &mut usize, until: char) -> Vec<char> {
     let k = *index;
     while *index < chars.len() && chars[*index] != until {
         *index += 1;
     }
-    chars[k..*index].iter().collect()
+    chars[k..*index].to_vec()
 }
 
 #[cfg(test)]
@@ -108,6 +107,14 @@ mod tests {
             (String::from("max_claim_chunks"), Value::Number(500)),
             (String::from("max_force_load_chunks"), Value::Number(25)),
         ]);
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn key_value_long() {
+        let input = String::from("{\n\t\tlast_login_time: 12345L\n}");
+        let actual = parse_snbt(input).unwrap();
+        let expected = HashMap::from([(String::from("last_login_time"), Value::Number(12345))]);
         assert_eq!(actual, expected);
     }
 }
